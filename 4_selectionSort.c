585102164f72a9f20eb9c5d487cd7f77bcfc366d
Pranto Bapary <pranto.bapary01@gmail.com>
2022-10-25 19:48:03 +0600

    add selection sort

---
+++ 4_selectionSort.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+void selectionSort(int arr[], int size)
+{
+    int temp, min, i, j;
+    for (i = 0; i < size - 1; i++)
+    {
+        min = i;
+        for (j = i + 1; j < size; j++)
+        {
+            if (arr[j] < arr[min])
+                min = j;
+        }
+        if (min != i)
+        {
+            temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
+int main()
+{
+    int size, i;
+    printf("Enter the size of the Array: ");
+    scanf("%d", &size);
+
+    int arr[size];
+    for (int i = 0; i < size; i++)
+        scanf("%d", &arr[i]);
+
+    selectionSort(arr, size);
+
+    for (i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    return 0;
+}
